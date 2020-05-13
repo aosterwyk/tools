9b3b7f8de276f2fa2e1dfd9040e1f5416d3bccff
Aaron <varixx@gmail.com>
2020-05-13 13:34:25 -0500

    bulk ffmpeg convert

---
+++ BulkFFMPEGConvert.ps1
@@ -0,0 +1,6 @@
+﻿# This script requires ffmpeg https://ffmpeg.org/download.html
+# The example below will convert all .mp4 files in a directory to .gif. You can change input and output formats if needed. 
+
+Get-ChildItem *.mp4 | ForEach-Object {
+    .\ffmpeg.exe -i "$($_.Name)" "$($_.BaseName).gif"
+}
