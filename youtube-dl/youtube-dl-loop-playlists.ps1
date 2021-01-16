5fae098ee4def51275debe99d0c02fa3265937e4
VariXx <varixx@gmail.com>
2021-01-16 15:32:20 -0600

    added archive file and output dir variables

--- youtube-dl/youtube-dl-loop-playlists.ps1
+++ youtube-dl/youtube-dl-loop-playlists.ps1
@@ -1,5 +1,8 @@
 # Requires youtube-dl in same directory 
 
+$outputDir = '.\' # Output directory. Each playlist will create a subdirectory. 
+$archiveName = 'downloaded.txt' # Filename for youtube-dl archive to track what episodes have been downloaded
+
 $playlists = @(
     'https://www.youtube.com/watch?v=z5EHJ0bxPfU&list=PLR6RS8PTcoXSbN9zeV13vR4MLbnr3n4IC',
     'https://www.youtube.com/watch?v=n9rUw-Jls8w&list=PLR6RS8PTcoXT_l1pSnRqzS1-btVp9DoNq'
@@ -7,6 +10,6 @@ $playlists = @(
 
 ForEach($pl in $playlists) {
     Write-Host $pl
-    .\youtube-dl.exe $pl -o '%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s'
+    .\youtube-dl.exe $pl -o "$($outputDir)%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s" --download-archive "$($archiveName)"
 }
 
