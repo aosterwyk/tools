562b6233869b874ca3fea94d64c6623f0d80c5de
VariXx <varixx@gmail.com>
2021-01-16 15:16:53 -0600

    added youtube-dl loop playlist script

---
+++ youtube-dl/youtube-dl-loop-playlists.ps1
@@ -0,0 +1,10 @@
+$playlists = @(
+    'https://www.youtube.com/watch?v=z5EHJ0bxPfU&list=PLR6RS8PTcoXSbN9zeV13vR4MLbnr3n4IC',
+    'https://www.youtube.com/watch?v=n9rUw-Jls8w&list=PLR6RS8PTcoXT_l1pSnRqzS1-btVp9DoNq'
+)
+
+ForEach($pl in $playlists) {
+    Write-Host $pl
+    .\youtube-dl.exe $pl -o '%(playlist_title)s/%(playlist_index)s - %(title)s.%(ext)s'
+}
+
