5f657d16fc96c7093317042ae6a3b8058be44ec0
VariXx <varixx@gmail.com>
2021-01-16 15:46:26 -0600

    added intructions comments

--- youtube-dl/youtube-dl-loop-playlists.ps1
+++ youtube-dl/youtube-dl-loop-playlists.ps1
@@ -1,9 +1,11 @@
-# Requires youtube-dl in same directory 
+# Requires youtube-dl in same directory - https://github.com/ytdl-org/youtube-dl/releases
 
-$outputDir = '.\' # Output directory. Each playlist will create a subdirectory. 
+$outputDir = '.\' # Output directory, use '.\' for same directory as the script. Each playlist will create a subdirectory. 
 $archiveName = 'downloaded.txt' # Filename for youtube-dl archive to track what episodes have been downloaded
-
+# Put playlist URLs surrounding by 's below. Do not use a comma after the last item.
 $playlists = @(
+    'https://www.youtube.com/watch?v=nAFIsPX3_3A&list=PLR6RS8PTcoXRKYa3LnG-PHeqLPAst4CHL',
+    'https://www.youtube.com/watch?v=FeeCKGYAenA&list=PLR6RS8PTcoXStXzjszhT1OpLWoqq0Zx0W',
     'https://www.youtube.com/watch?v=z5EHJ0bxPfU&list=PLR6RS8PTcoXSbN9zeV13vR4MLbnr3n4IC',
     'https://www.youtube.com/watch?v=n9rUw-Jls8w&list=PLR6RS8PTcoXT_l1pSnRqzS1-btVp9DoNq'
 )
