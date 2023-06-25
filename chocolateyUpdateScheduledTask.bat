4612fb2d0bd7c7488a09b5a5816e911151966019
Aaron <mail@aosterwyk.com>
2023-06-24 23:34:50 -0500

    updated update command

--- chocolateyUpdateScheduledTask.bat
+++ chocolateyUpdateScheduledTask.bat
@@ -1,3 +1,3 @@
 REM run as admin
 @echo off
-schtasks /create /tn "Chocolatey Update" /tr "cup all -y" /sc daily /st 04:00 /ru System 
+schtasks /create /tn "Chocolatey Update" /tr "choco upgrade all -y" /sc daily /st 04:00 /ru System 
